98b280951c7523cb9feae43104e78c62fcaa6fd6
mpt <mpt@elca.vn>
2021-04-09 19:11:09 +0700

    Init Exercise

---
+++ EmployeeManagement/EmployeeManagement.UnitTest/EmployeeServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeManagement.UnitTest
+{
+    [TestClass]
+    public class EmployeeServiceTest
+    {
+        [TestMethod]
+        public void CalculateSalaryEngineerTestSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/EmployeeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EmployeeManagement
+{
+    public class EmployeeService
+    {
+        private const decimal BASIC_SALARY = 10000000;
+
+        public static decimal CalculateSalary(EmployeeType employeeType, int years)
+        {
+            if (years < 0)
+            {
+                throw new ArgumentException("Years of experience is not valid");
+            }
+
+            decimal factor = 0;
+
+            switch (employeeType)
+            {
+                case EmployeeType.Engineer:
+                    factor = 1;
+                    break;
+                case EmployeeType.SeniorEngineer:
+                case EmployeeType.Manager:
+                    factor = 1.5m;
+                    break;
+                case EmployeeType.SeniorManager:
+                    factor = 2;
+                    break;
+                case EmployeeType.Expert:
+                    factor = 2.8m;
+                    break;
+            }
+
+            var salary = BASIC_SALARY * years * factor;
+
+            return salary;
+        }
+    }
+}
